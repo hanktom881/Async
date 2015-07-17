8c7519405166f0c40610647e72ffb44bbf89fa5f
Hank Tom <hanktom@gmail.com>
2015-07-17 14:58:18 +0800

    Job2Task, Job3Task類別設計

--- app/src/main/java/com/tom/async/MainActivity.java
+++ app/src/main/java/com/tom/async/MainActivity.java
@@ -20,14 +20,67 @@ public class MainActivity extends Activity {
 
     public void go(View v){
         Log.d("JOB1", "sleeping");
-        new Job1Task().execute();
+//        new Job1Task().execute();
+//        new Job2Task().execute(3);
+        new Job3Task().execute(5);
+    }
+
+    class Job3Task extends AsyncTask<Integer, Integer, Void>{
+
+        @Override
+        protected Void doInBackground(Integer... params) {
+            int n = params[0];
+            for (int i=n; i>0; i--){
+                publishProgress(i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            TextView info = (TextView) findViewById(R.id.info);
+            info.setText("DONE");
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            TextView info = (TextView) findViewById(R.id.info);
+            info.setText(values[0]+"");
+        }
+    }
+
+    class Job2Task extends AsyncTask<Integer, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Integer... params) {
+
+            try {
+                Thread.sleep(params[0]*1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            TextView info = (TextView) findViewById(R.id.info);
+            info.setText("DONE");
+        }
     }
 
     class Job1Task extends AsyncTask<Void, Void, Void>{
         @Override
         protected Void doInBackground(Void... params) {
             try {
-
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
